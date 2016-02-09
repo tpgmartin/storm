4d88d21a26e800c48d5a1e78620f0fe17c74bfeb
Tom Martin <tom@tpgmartin.com>
2016-02-09 23:23:22 +0000

    Create checks for null

--- src/assertion.js
+++ src/assertion.js
@@ -4,4 +4,19 @@ function Assertion (name, expectedValue, contextName) {
   this.contextName   = contextName;
 }
 
+// Should be able to handle excpetions when checking assertions
+// Add some logging
+
+Assertion.prototype.isTrue = function () {
+  return this.expectedValue === true;
+}
+
+Assertion.prototype.isFalse = function () {
+  return this.expectedValue === false;
+}
+
+Assertion.prototype.isNull = function () {
+  return this.expectedValue === null;
+}
+
 module.exports = Assertion;
--- test/assertion.spec.js
+++ test/assertion.spec.js
@@ -1,5 +1,8 @@
 var should = require('chai').should();
+var Context = require('../src/context');
 var Assertion = require('../src/assertion');
+
+// Add before each
 
 describe('Assertion', function () {
   describe('creating a new assertion instance', function () {
@@ -16,5 +19,51 @@ describe('Assertion', function () {
         output.expectedValue.should.equal(true);
         output.contextName.should.equal('input_context');
     });  
+  });
+  describe('checking an assertion', function () {
+    it('should return "true" for a correst "isTrue" assertion', function () {
+        var input_context = new Context ('context_name', function cb () {});
+        
+        var output = input_context.asserts('my_assertion', true).isTrue();
+        
+        output.should.be.a('boolean');
+        output.should.equal(true);
+    });
+    
+    it('should return "false" for an incorrest "isTrue" assertion', function () {
+        var input_context = new Context ('context_name', function cb () {});
+        
+        var output = input_context.asserts('my_assertion', false).isTrue();
+        
+        output.should.be.a('boolean');
+        output.should.equal(false);
+    });
+    
+    it('should return "true" for a correst "isFalse" assertion', function () {
+        var input_context = new Context ('context_name', function cb () {});
+        
+        var output = input_context.asserts('my_assertion', false).isFalse();
+        
+        output.should.be.a('boolean');
+        output.should.equal(true);
+    });
+    
+    it('should return "true" for a correst "isNull" assertion', function () {
+        var input_context = new Context ('context_name', function cb () {});
+        
+        var output = input_context.asserts('my_assertion', null).isNull();
+        
+        output.should.be.a('boolean');
+        output.should.equal(true);
+    });
+    
+    it('should fail silently if not called with assetion method', function () {
+        var input_context = new Context ('context_name', function cb () {});
+        
+        var output = input_context.asserts('my_assertion', true);
+        
+        output.should.be.an('object');
+    });
+    
   });
 });
